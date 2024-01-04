01960ae18963d347c6255db7c80ef1f77573d93a
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-01-04 16:03:39 +0530

    fix:added messages controller

--- NotesBackend/controllers/messageCntrl.js
+++ NotesBackend/controllers/messageCntrl.js
@@ -1,7 +1,7 @@
 const Message = require('../models/messagesModel');
 const Community = require('../models/communityModel');
 
-const createMessage = async (req, res) => { 
+const createMessage = async (req, res) => {
 
     try {
         const { content, community } = req.body;
@@ -22,6 +22,9 @@ const createMessage = async (req, res) => {
     }
 }
 
+
+
+
 const getCommunityMessages = async (req, res) => {
 
     try {
@@ -30,14 +33,46 @@ const getCommunityMessages = async (req, res) => {
         if (!community) {
             return res.status(400).json({ error: "No such community exists" });
         }
-        for(const message of community.messages){
+        for (const message of community.messages) {
             await message.populate('sender', 'username profile')
         }
-        
+
         res.status(200).json({ messages: community.messages });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
-module.exports = { createMessage, getCommunityMessages };
+
+
+const deleteMessages = async (req, res) => {
+    const { messageId } = req.params;
+    const userID = req.user.id;
+    try {
+        const message = await Message.findById(messageId);
+        if (!message) {
+            return res.status(400).json({ error: "No such message exists" });
+        }
+
+        if (message.sender.toString() !== userID) {
+            return res.status(400).json({ error: "You are not authorized to delete this message" });
+        }
+
+        const community = await Community.findById(message.community);
+        if (!community) {
+            return res.status(400).json({ error: "No such community exists" });
+        }
+        const index = community.messages.indexOf(messageId);
+        if (index > -1) {
+            community.messages.splice(index, 1);
+        }
+        await community.save();
+        await message.remove();
+        res.status(200).json({ message: "Message deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
+module.exports = { createMessage, getCommunityMessages, deleteMessages };
