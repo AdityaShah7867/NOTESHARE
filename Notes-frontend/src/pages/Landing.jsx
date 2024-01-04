4ca7be140e00fd745c1e5b49f66aca82805e5ed4
Aditya <192105adityashah@gmail.com>
2024-01-04 19:14:34 +0530

    new ui

--- Notes-frontend/src/pages/Landing.jsx
+++ Notes-frontend/src/pages/Landing.jsx
@@ -44,8 +44,8 @@ const Landing = () => {
                 </div>
                 <div className="navmenu hidden w-full flex-wrap justify-end items-center mb-16 space-y-8 p-6 border border-gray-100 rounded-3xl shadow-2xl shadow-gray-300/20 bg-white dark:bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none dark:shadow-none dark:border-gray-700 lg:border-0">
                   <div className="text-gray-600 dark:text-gray-300 lg:pr-4">
-                    <ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0">
-                      <li>
+                    {/*<ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0">
+                       <li>
                         <a
                           href="#"
                           className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight"
@@ -69,7 +69,7 @@ const Landing = () => {
                           <span>Services</span>
                         </a>
                       </li>
-                    </ul>
+                    </ul> */}
                   </div>
                   <div className="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
                   <NavLink to='/register'>
